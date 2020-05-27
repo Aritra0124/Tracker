b99bcf0c044b3e084a23b7701b99482ba0311300
aritra <aritrachakraborty0124@gmail.com>
2020-05-27 20:50:58 +0530

    Changes done. 1. Minor alignment fixing. 2. Minor SQL Query fixing.
    Issues
    1. Blank input.
    2. Add delete option.
    3. Add time counter.
    4. Mobile view fix.

---
+++ static/css/graph-handler.js
@@ -0,0 +1,42 @@
+function renderChart() {
+    var ctx = document.getElementById('myChart');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(75, 192, 192, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 3
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
+
+$("#renderBtn").click(
+    function () {
+//        data = [20000, 14000, 12000, 15000, 18000, 19000, 22000];
+        //       labels =  ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+        renderChart();
+    }
+);
